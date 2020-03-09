90a17309d8968ad46177070d851d0486cd766768
Anatoly Popov <trudovoe.popov@gmail.com>
2020-03-09 14:41:05 +0300

    28 урок

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,20 +2,11 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-// Данные
-
-let posts = [
-    {id: 1, likeCount: 22, post: 'Commodi earum eligendi impedit itaque nisi nostrum placeat? Debitis delectus eos laborum, quae saepe tempore? Architecto molestiae nostrum quibusdam sunt vel! Quibusdam.'},
-    {id: 2, likeCount: 13, post: 'Aut illum impedit sit tenetur. Aliquam beatae consectetur, corporis eaque, est excepturi in iure laboriosam laudantium minima possimus reprehenderit sunt tenetur, ut.'},
-]
-
-// Проходимся .map по массиву данных и создаем массив jsx элементов
-
-let postsElements = posts.map(p => <Post message={p.post} likeCount={p.likeCount} key={p.id} />);
-
-// Создаются компоненты, через props они получают данные из атрибутов
-
 const MyPosts = (props) => {
+
+    // Проходимся .map по массиву данных и создаем массив jsx элементов. Далее передаем их через пропсы в нужные компоненты.
+    let postsElements = props.posts.map(p => <Post message={p.post} likeCount={p.likeCount} key={p.id} />);
+
     return (
         <div className={s.posts}>
             <div className={s.postsTitle}>
