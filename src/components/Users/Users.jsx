0a18333cb283f52e4c8c8ba0a26fd5bd080d302f
Anatoly Popov <trudovoe.popov@gmail.com>
2020-04-13 21:59:48 +0300

    55.1 урок. Pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,43 @@ import * as axios from 'axios';
 
 import userDefaultMale from '../../assets/images/userDefaultMale.png';
 
-class Users extends React.Component {
+class UsersAPICOmponent extends React.Component {
 
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            this.props.setUsers(response.data.items)
-        });
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                    this.props.setUsers(response.data.items);
+                    this.props.setTotalUsersCount(response.data.totalCount);
+                }
+            );
     }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    };
+
     render(){
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+
+        for(let i=1; i <= pagesCount; i++){
+            pages.push(i);
+        }
+
         return <div className={s.usersContainer}>
+            <div className={s.pagination}>
+                {pages.map(p =>{
+                    return <span
+                        className={this.props.currentPage === p && s.activePage}
+                        onClick={ (e) => { this.onPageChanged(p) } }
+                        >{p}</span>
+                })}
+            </div>
             {
                 this.props.users.map(u =>
                     <div className={s.user} key={u.id}>
@@ -39,4 +66,4 @@ class Users extends React.Component {
     }
 }
 
-export default Users;
+export default UsersAPICOmponent;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import {followAC, unfollowAC, setUsersAC} from "../../redux/users-reducer";
-import Users from "./Users";
+import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setUsersotalCountAC} from "../../redux/users-reducer";
+import UsersAPICOmponent from "./Users";
 import {connect} from "react-redux";
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 };
 
@@ -19,10 +22,16 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (CurrentPage) => {
+            dispatch(setCurrentPageAC(CurrentPage))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setUsersotalCountAC(totalCount))
         }
     }
 };
 
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPICOmponent);
 
 export default DialogsContainer;
