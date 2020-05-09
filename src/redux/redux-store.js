1c689d20c6baab513970bca66df5f0b91917e6a0
Anatoly Popov <trudovoe.popov@gmail.com>
2020-05-09 16:21:19 +0300

    75 Урок. Форма логина redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,15 +3,17 @@ import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import navbarReducer from './navbar-reducer';
 import usersReducer from './users-reducer';
-import authReducer from "./auth-reducer";
+import authReducer from './auth-reducer';
 import thunkMiddleware from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form';
 
 const reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     navbar: navbarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
