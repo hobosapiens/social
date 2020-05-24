785d91af2f161a5744d3466f2b2b3c3cd8e94d5a
Anatoly Popov <trudovoe.popov@gmail.com>
2020-05-24 23:12:35 +0300

    Урок 91. extensions

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -19,12 +19,8 @@ const reducers = combineReducers({
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-    reducers,
-    composeEnhancer(applyMiddleware(thunkMiddleware)),
-);
-
-window.store = store;
+const store = createStore(reducers, composeEnhancer(
+    applyMiddleware(thunkMiddleware)
+));
 
 export default store;
