cc86b0376ea0d0b6aba1c12171b53c423896dc55
Anatoly Popov <trudovoe.popov@gmail.com>
2020-05-27 01:06:37 +0300

    Урок 92-2. react-test-renderer

--- src/components/Profile/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileStatus.test.jsx
@@ -9,16 +9,42 @@ describe('ProfileStatus component', () => {
         expect(instance.state.status).toBe('test_status');
     });
 
-    test('After creation span should be displayed', () => {
+    test('After creation <span> should be displayed', () => {
         const component = create(<ProfileStatus status='test_status' />);
-        const instance = component.getInstance();
-        let span = instance.findByType('span');
-        expect(span.length).toBe(1);
+        const root = component.root;
+        let span = root.findByType('span');
+        expect(span).not.toBeNull();
     });
-    test('After creation span should contain correct status', () => {
+
+    test('After creation <input> should not be displayed', () => {
         const component = create(<ProfileStatus status='test_status' />);
+        const root = component.root;
+        expect(() => {
+            let input = root.findByType('input');
+        }).toThrow();
+    });
+
+    test('After creation <span> should contain correct status', () => {
+        const component = create(<ProfileStatus status='test_status' />);
+        const root = component.root;
+        let span = root.findByType('span');
+        expect(span.children[0]).toBe('test_status');
+    });
+
+    test('Input should be desplayed in edit mode instead span', () => {
+        const component = create(<ProfileStatus status='test_status' />);
+        const root = component.root;
+        let span = root.findByType('span');
+        span.props.onDoubleClick();
+        let input = root.findByType('input');
+        expect(input.props.value).toBe('test_status');
+    });
+
+    test('callback should be called', () => {
+        const mockCallback = jest.fn();
+        const component = create(<ProfileStatus status='test_status' updateStatus={mockCallback} />);
         const instance = component.getInstance();
-        let span = instance.findByType('span');
-        expect(span.length).toBe(1);
+        instance.deactivateEditMode();
+        expect(mockCallback.mock.calls.length).toBe(1);
     });
 });
