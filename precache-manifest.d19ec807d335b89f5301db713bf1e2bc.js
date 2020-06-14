2e205bc3bdc10aedb374d1cfaa9027f98c1b7fcc
Anatoly Popov <trudovoe.popov@gmail.com>
2020-06-15 01:54:53 +0300

    Updates

---
+++ precache-manifest.d19ec807d335b89f5301db713bf1e2bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81093e961833a7356904ac7121c3e430",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "60ec991cd5259769a826",
+    "url": "/social/static/css/3.c89d5f15.chunk.css"
+  },
+  {
+    "revision": "6c2f4d6eb28f1f07b241",
+    "url": "/social/static/css/4.a91e9b34.chunk.css"
+  },
+  {
+    "revision": "323d959f09b6f0a6f404",
+    "url": "/social/static/css/main.10de0165.chunk.css"
+  },
+  {
+    "revision": "38e635154ed24e3e8447",
+    "url": "/social/static/js/2.f4289623.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/social/static/js/2.f4289623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ec991cd5259769a826",
+    "url": "/social/static/js/3.273090d3.chunk.js"
+  },
+  {
+    "revision": "6c2f4d6eb28f1f07b241",
+    "url": "/social/static/js/4.00bf3bf8.chunk.js"
+  },
+  {
+    "revision": "323d959f09b6f0a6f404",
+    "url": "/social/static/js/main.cc5e8db6.chunk.js"
+  },
+  {
+    "revision": "c47dedcf66e2abeb7284",
+    "url": "/social/static/js/runtime-main.08b3aabb.js"
+  },
+  {
+    "revision": "518090cc52eeeb6214fac2233ea230cd",
+    "url": "/social/static/media/jobIcon.518090cc.svg"
+  },
+  {
+    "revision": "b0dbb95997333c85d9b5ae3272610302",
+    "url": "/social/static/media/logout.b0dbb959.svg"
+  },
+  {
+    "revision": "949d9d44d68ecdfa194c82e8b099d8cd",
+    "url": "/social/static/media/preloader.949d9d44.svg"
+  },
+  {
+    "revision": "07f6f57517a78ee4cac6457552327b26",
+    "url": "/social/static/media/userDefaultMale.07f6f575.png"
+  }
+]);
